ef3b975506395743d4cf00ce08e2900dc333672d
RUTUJA AHIRE <rutujaahire.er01@gmail.com>
2026-05-10 11:25:11 +0530

    Add files via upload

---
+++ function.py
@@ -0,0 +1,19 @@
+#name=["sweety","priya","ritu","uma","tanu"]
+#exam=["jee","neet","hsc","mhtcet","iser"]
+#def name_len():
+ #   print(len(name))
+  #  print(len(exam))
+#name_len()
+#def sub():
+ #   for i in name:
+  #      print(i ,end="\n")
+   # for j in exam:
+    #    print(j,en))
+def cal_fact(n):
+    fact=1
+    for i in range(1,n+1):
+        fact*=i
+        print(fact)
+cal_fact(8)
+
+
